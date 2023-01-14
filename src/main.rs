58232689a22180832a0b50e3507bbb526a5e85ca
hootisman <david1zalewski@gmail.com>
2023-01-13 21:26:50 -0500

    added unusable linked list code dont use this :(

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+/*
+struct Jaja {
+    value: i32,
+    value2: String,
+}
+
+*/
+/* 
+ *
+ * Linked List
+ *
+ */
+use std::mem;
+
+pub struct List {
+    head: Link,
+}
+enum Link {
+    None,
+    Some(Box<Node>),
+}
+struct Node {
+    value: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::None }
+    }
+    pub fn push(&mut self,n:i32){
+        let new_node = Box::new(Node {
+            value: n,
+            next: mem::replace(&mut self.head, Link::None),
+        });
+        self.head = Link::Some(new_node)
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let list1: List = List::new();
 }
